f7655ce4c519a2ee777c2f316efc3f59d0b9ddf2
Vishal Sindham <vszb101@gmail.com>
2022-09-07 01:12:27 -0400

    hosting: hosting the application to access publically and showcase

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2$doc$=d(^86z(*2x67zcz-vl(s*g9j(4g9p__l!60m%-osd0f'
+# SECRET_KEY = "django-insecure-2$doc$=d(^86z(*2x67zcz-vl(s*g9j(4g9p__l!60m%-osd0f"
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY','2$doc$=d(^86z(*2x67zcz-vl(s*g9j(4g9p__l!60m%-osd0f']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG','') != 'False'
+ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
@@ -125,15 +126,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles') 
 STATIC_URL = '/static/'
-
+MEDIA_URL = '/static/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR ,'static')
 ]
-
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles') 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ devsearch/staticfiles/js/main.cd40e81223ae.js
@@ -0,0 +1,27 @@
+    // Get search form and page links 
+    let searchForm = document.getElementById("searchForm");
+    let pageLinks = document.getElementsByClassName("page-link");
+
+    // ensure search form exists
+    if(searchForm){
+
+    for(let i = 0; pageLinks.length > i; i++ ){
+        pageLinks[i].addEventListener('click', function (e) {
+            e.preventDefault();
+            // console.log('Button click');
+
+            // Get the data attribute
+            let page = this.dataset.page;
+            // console.log('PAGE:', page);
+
+            // Add hidden search input to the form 
+            searchForm.innerHTML += `<input value=${page} name="page" hidden/>` ;
+
+            // Submit form
+            searchForm.submit()
+        });
+    }
+
+    }
+
+
